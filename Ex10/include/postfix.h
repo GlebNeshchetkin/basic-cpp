6eda88460fbfcb8e848999d67c75531c394183e8
GlebNeshchetkin <glebneshchetkin@gmail.com>
2023-09-19 18:38:54 +0300

    labs 7,8,9,10 added

---
+++ Ex10/include/postfix.h
@@ -0,0 +1,9 @@
+// Copyright 2020 GN
+#ifndef INCLUDE_POSTFIX_H_
+#define INCLUDE_POSTFIX_H_
+#include <iostream>
+#include <string>
+bool isOperator(char c);
+int getPriority(char C);
+std::string infix2postfix(std::string infix);
+#endif  // INCLUDE_POSTFIX_H_
